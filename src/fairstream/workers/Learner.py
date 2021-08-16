31197827f5f9e6ec6bc229b687a9ae7eb5f3c7ce
Jiaxing Joy Qiu <52210089+JiaxingQiu@users.noreply.github.com>
2021-08-16 07:43:18 -0400

    Delete src directory

--- src/fairstream/workers/Learner.py
+++
@@ -1,6 +0,0 @@
-from fairstream.workers.Goblin import Goblin
-
-
-class Learner(Goblin):
-    def __init__(self, work_dir):
-        Goblin.__init__(self, work_dir)
--- src/fairstream/workers/Reporter.py
+++
@@ -1,6 +0,0 @@
-from fairstream.workers.Goblin import Goblin
-
-
-class Reporter(Goblin):
-    def __init__(self, work_dir):
-        Goblin.__init__(self, work_dir)
